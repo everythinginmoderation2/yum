261f47ed92c118c854791e797247b4b2af6b240d
unknown <everythinginmoderation2@gmail.com>
2021-06-03 00:36:10 +0700

    cleared clutter

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -3,20 +3,6 @@ import "./App.css";
 import BusinessList from "../BusinessList/BusinessList";
 import SearchBar from "../SearchBar/SearchBar";
 
-// const business = {
-//   imageSrc: 'https://content.codecademy.com/programs/react/ravenous/pizza.jpg',
-//   name: 'MarginOtto Pizzeria',
-//   address: '1010 Paddington Way',
-//   city: 'Flavortown',
-//   state: 'NY',
-//   zipCode: '10101',
-//   category: 'Italian',
-//   rating: 4.5,
-//   reviewCount: 90
-// }
-
-// const buinesses = [business, business, business, business, business, business]
-
 const App = () => {
   const API_KEY = process.env.REACT_APP_YELP_API_KEY;
   // const CLIENT_ID = process.env.REACT_APP_YELP_CLIENT_ID;
@@ -55,24 +41,6 @@ const App = () => {
     }
   };
 
-//   const timeToEat = (term, location, sortBy) => {
-//     return fetch(
-//       `https://cors-anywhere.herokuapp.com/https://api.yelp.com/v3/businesses/search?term=${term}&location=${location}&sort_by=${sortBy}`,
-//       { headers: { Authorization: `Bearer ${API_KEY}` } },
-//     ).then((response) => {
-//       return response.json()
-//     }).then((jsonResponse) => {
-//       if (jsonResponse.businesses){
-//         setBusinesses(jsonResponse.businesses)
-//       }
-//     })
-// };
-
-  // function searchYelp(term, location, sortBy) {
-  //   console.log(`Searching Yelp with ${term}, ${location}, ${sortBy} 👌`);
-  //   console.log(businesses);
-  //   timeToEat(term, location, sortBy);
-  // }
 
   return (
     <div className="App">
